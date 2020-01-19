9b1941c504358d9a53051aa3ef3a328bd3a79afc
chagall831117 <chagall831117@users.noreply.github.com>
2020-01-19 19:16:01 +0800

    完成

---
+++ Unity_TeleportAndFade/Assets/Opening.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Opening : MonoBehaviour
+{
+    public Image FadImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadImg = GetComponent<Image>();
+        StartCoroutine(FadIn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator FadIn()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            FadImg.color -= new Color(0, 0, 0, 0.05f);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    }
+}
---
+++ Unity_TeleportAndFade/Assets/Teleport.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Teleport : MonoBehaviour
+{
+    public Collider Cube;
+    public Image FadImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cube = GameObject.Find("Cube").GetComponent<Collider>();
+        FadImg = GameObject.Find("轉場效果").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "玩家")
+        {
+            StartCoroutine((Faded()));
+            
+        }
+    }
+    private IEnumerator Faded()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            FadImg.color += new Color(0, 0, 0, 0.05f);
+            yield return new WaitForSeconds(0.05f);
+        }
+        SceneManager.LoadScene("第二關");
+    }
+}
